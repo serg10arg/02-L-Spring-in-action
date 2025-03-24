839bec256097ce23984d619d360667cb193be54b
serg10arg <cabrerasergio094@gmail.com>
2025-03-24 08:46:11 -0300

    spring-in-action

---
+++ 01-Foundational-Spring/02-Proyectos/04-Trabajando-con-datos-no-relacionales/01-Tacos-sd-Cassandra/src/test/java/com/example/__Tacos_sd_Cassandra/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.__Tacos_sd_Cassandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
